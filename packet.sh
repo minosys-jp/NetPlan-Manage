dbfdbf5f37452e8f0f6027c1c3343200e6fa0e44
Minoru Matsumoto <minoru.matsumoto@westgate.co.jp>
2025-09-15 14:11:10 +0900

    changed from public to dist directory

--- packet.sh
+++ packet.sh
@@ -3,6 +3,9 @@
 if [ ! -d "packet" ]; then
 	mkdir packet
 fi
-cp -rfp public packet
+if [ ! -d "packet/public" ]; then
+	mkdir "packet/public"
+fi
+cp -rfp dist/* packet/public/
 cp -fp server.js packet
 cp -fp start.sh packet
